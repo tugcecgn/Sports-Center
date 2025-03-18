c7bc2d4984b44035d67b9d0154aa9953b045ffff
Tuğçe İçigen <tugceicigen0917@gmail.com>
2025-03-18 18:25:08 +0300

    bölümler eklendi iyileştirmeler kesinlikle yapılmalı

--- js/main.js
+++ js/main.js
@@ -11,3 +11,33 @@ window.onscroll = function(){
     }
 
 }
+const title = [
+    "Why are Your Yoga?",
+    "Why you shouldn't work with a group",
+    "Train Solo, Shine Strong!",
+    "Stretch Your Limits",
+]
+const images = [
+    "./images/yoga.jpg",
+    "./images/group.webp",
+    "./images/solo.jpg",
+    "./images/stret.webp",  
+]
+const time = [
+    "When comes Your Time Yoga",
+    "When comes Your Time Group",
+    "When comes Your Time Solo",
+    "When comes Your Time Stretching",
+]
+function changeContent(index,clickedbuton){
+    document.getElementById("content-title").textContent=title[index];
+    document.getElementById("content-images").src=images[index];
+    document.getElementById("content-time").textContent=time[index];
+
+    document.querySelectorAll(".btn").forEach(button =>{
+        button.classList.remove("active");
+    });
+    clickedbuton.classList.add("active");
+
+
+}
