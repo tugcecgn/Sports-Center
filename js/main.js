939b709de880447bcc446b96333030632172a286
Tuğçe İçigen <tugceicigen0917@gmail.com>
2025-03-19 23:01:18 +0300

    sadece 1 animasyon  kaldı

--- js/main.js
+++ js/main.js
@@ -45,3 +45,44 @@ function changeContent(index,clickedButton){
 
 
 }
+
+function calculateBMI() {
+    let heightInput = document.getElementById("height");
+    let weightInput = document.getElementById("weight");
+
+    let height = parseFloat(heightInput.value) / 100; // cm -> metre
+    let weight = parseFloat(weightInput.value);
+
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+        alert("Lütfen geçerli bir boy ve kilo girin!");
+        return;
+    }
+
+    let bmi = weight / (height * height);
+    console.log("Hesaplanan BMI:", bmi);
+
+    // BMI değerine göre oku konumlandırma (yaklaşık yüzdelik hesaplandı)
+    let position = 0;
+
+    if (bmi < 18.5) {
+        position = 20; // Zayıf
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        position = 32; // Normal
+    } else if (bmi >= 25 && bmi < 29.9) {
+        position = 47; // Fazla kilolu
+    } else if (bmi >= 30 && bmi < 34.9) {
+        position = 62;
+    }else {
+        position = 76; // Obez
+    }
+
+    document.getElementById("bmi-arrow").style.left = position + "%";
+}
+
+// Enter tuşuna basıldığında hesaplama yap
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Sayfanın yenilenmesini önler
+        calculateBMI();
+    }
+});
